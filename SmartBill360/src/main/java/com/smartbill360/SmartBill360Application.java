59949bc79bbd6ebd8498f4ccd6c157a4b0cb2642
mehul1908 <135810448+mehul1908@users.noreply.github.com>
2025-07-10 09:37:26 +0530

    initial setup

---
+++ SmartBill360/src/main/java/com/smartbill360/SmartBill360Application.java
@@ -0,0 +1,13 @@
+package com.smartbill360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBill360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBill360Application.class, args);
+	}
+
+}
---
+++ SmartBill360/src/test/java/com/smartbill360/SmartBill360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbill360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBill360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
