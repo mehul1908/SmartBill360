bc5832c87114bc7c3bf9d6b063fd7b0d89dadb84
mehul1908 <135810448+mehul1908@users.noreply.github.com>
2025-08-01 17:15:03 +0530

    update apis

--- SmartBill360/src/main/java/com/smartbill360/entity/User.java
+++ SmartBill360/src/main/java/com/smartbill360/entity/User.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
